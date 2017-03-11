171ab9b2a04135f352fc4cff9142a646719b029c
Rex Rockwell <rexjrockwell@outlook.com>
2017-03-10 19:38:08 -0800

    Add horizontal movement and animation to player character
    - Replace robot prefab on stage with a new Player prefab composed of the
    Robot model and rig
    - Add an Animator to handle state changes of the robot
    - Add InputHandler and CharacterMotion scripts to control robot
    walking/idle states in regards to motion and animation
    - Move Robo assets to Prefabs folder

---
+++ Assets/Source/CharacterMotion.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class CharacterMotion : MonoBehaviour
+{
+    private const float _walkSpeed = 0.1f;
+
+    // Characters should face slightly toward the camera instead of directly left or right
+    private const float _leftFacingRotation = 240;
+    private const float _rightFacingRotation = 120;
+
+    private CharacterController _controller;
+    private Animator _animator;
+
+    private float _horizontalMovement;
+
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+        _animator = GetComponent<Animator>();
+
+        _horizontalMovement = 0;
+    }
+
+    void FixedUpdate()
+    {
+        if (_horizontalMovement != 0)
+        {
+            _animator.SetBool("moving", true);
+
+            if (_horizontalMovement < 0)
+            {
+                _controller.transform.eulerAngles = new Vector3(0, _leftFacingRotation, 0);
+            }
+            else
+            {
+                _controller.transform.eulerAngles = new Vector3(0, _rightFacingRotation, 0);
+            }
+
+            _controller.Move(new Vector3(_horizontalMovement, 0, 0));
+
+            // Clear the movement vector after new movement has been applied
+            _horizontalMovement = 0;
+        }
+    }
+
+    public void MoveLeft()
+    {
+        _horizontalMovement -= _walkSpeed;
+    }
+
+    public void MoveRight()
+    {
+        _horizontalMovement += _walkSpeed;
+    }
+
+    public void HorizontalMovementStopped()
+    {
+        _animator.SetBool("moving", false);
+    }
+}
---
+++ Assets/Source/InputHandler.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    private const string _playerObjectTag = "Player";
+    private const string _movementAxis = "Horizontal";
+    private const string _jumpAxis = "Jump";
+
+    private CharacterMotion _playerMotion;
+
+	void Start()
+    {
+        _playerMotion = GameObject.FindGameObjectWithTag(_playerObjectTag).GetComponent<CharacterMotion>();
+	}
+
+    void Update()
+    {
+        float movementDirection = Input.GetAxisRaw(_movementAxis);
+        if (movementDirection < 0)
+        {
+            _playerMotion.MoveLeft();
+        }
+        else if (movementDirection > 0)
+        {
+            _playerMotion.MoveRight();
+        }
+        else
+        {
+            _playerMotion.HorizontalMovementStopped();
+        }
+    }
+}
