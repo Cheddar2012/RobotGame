b33a9fc90cd3267914061666b6aa524504fa09c6
Rex Rockwell <rexjrockwell@outlook.com>
2017-03-12 21:02:59 -0700

    Create title screen
    - Create title screen with stationary character and button to load game
    scene
    - Import material for title screen floor
    - Make material based on imported material for gameplay floor

---
+++ Assets/Source/ButtonActions.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonActions : MonoBehaviour
+{
+    public void LoadTitleScreen()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
