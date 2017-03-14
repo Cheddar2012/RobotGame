8f427dbeca2838de0978c24b0f2c11721a68192f
Rex Rockwell <rexjrockwell@outlook.com>
2017-03-14 01:36:34 -0700

    Add platforming gameplay
    - Build out level to encourage player to utilize the game mechanics
    - Populate with multiple turret enemies
    - Add additional rocket pickup locations
    - Add boss turret that fires quickly at a long distance

---
+++ Assets/Source/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _target;
+
+    private Vector3 _lastTargetPosition;
+
+    private void Start()
+    {
+        _lastTargetPosition = _target.position;
+    }
+    
+	private void Update()
+    {
+        Vector3 targetPositionChange = _target.position - _lastTargetPosition;
+        transform.position += new Vector3(targetPositionChange.x, targetPositionChange.y, 0);
+        _lastTargetPosition = _target.position;
+    }
+}
